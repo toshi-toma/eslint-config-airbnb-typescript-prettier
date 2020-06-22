19d62cb99cd52508918c91cabfef43a94215b715
toshi-toma <toshitoma1217@gmail.com>
2020-06-23 00:48:35 +0900

    feat!: Update typescript-eslint to v3
    BREAKING CHANGE: enable new rules in typescript-eslint/recommended

--- index.js
+++ index.js
@@ -28,9 +28,6 @@ module.exports = {
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/explicit-member-accessibility": "off",
     "@typescript-eslint/no-object-literal-type-assertion": "off",
-    "@typescript-eslint/prefer-interface": "off",
-    "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
     // React
     "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
     "react/prop-types": ["off", {}],

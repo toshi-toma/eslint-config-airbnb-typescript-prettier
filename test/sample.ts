319683cd43500e73140faae765bb88d0399ba6f3
Dominic Garms <djgarms@gmail.com>
2020-11-21 22:43:24 +0800

    fix: use no-shadow of @typescript-eslint instead of base rule (#71)

--- test/sample.ts
+++ test/sample.ts
@@ -1,16 +1,19 @@
+import { Person, SampleData } from "./sampleTypes";
+
 function sum(a: number, b: number) {
   return a + b;
 }
 
 sum(1, 2);
 
-interface Person {
-  name: string;
-  age: number;
-}
-
 function greeter(person: Person) {
   return `My name is ${person.name}. I am ${person.age} years old.`;
 }
 
 greeter({ name: "toshi-toma", age: 24 });
+
+function greeterWithEnum(person: SampleData) {
+  return `My name is ${person.name}. I am ${person.age} years old.`;
+}
+
+greeterWithEnum({ name: SampleData.name, age: SampleData.age });
---
+++ test/sampleTypes.ts
@@ -0,0 +1,9 @@
+export interface Person {
+  name: string;
+  age: number;
+}
+
+export const enum SampleData {
+  name = "toshi-toma",
+  age = 24,
+}
